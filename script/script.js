a50a76c6e721ef0a0aeb8352b7ce931fa8dbfe31
Hiroim Nakano <nakanohiromi818@gmail.com>
2021-04-22 21:57:20 -0700

    jQuery done

--- script/script.js
+++ script/script.js
@@ -1,11 +1,28 @@
-// $(document).ready(function() {
-//   $('a[href^="#"]').click(function(){
-//     var speed = 500;
-//     var href= $(this).attr("href");
-//     var target = $(href == "#" || href == "" ? 'html' : href);
-//     var position = target.offset().top;
-//     $("html, body").animate({scrollTop:position}, speed, "swing");
-//     return false;
-//   });
-  
-// })
+$(document).ready(function() {
+  // Top背景画像のスライダー
+  $('.top_slider').bgSwitcher({
+    images: ['../images/header-top-image.png','../images/bedroom-image.png','../images/room-image.png'],
+    Interval: 5000, 
+      start: true, 
+      loop: true, 
+      shuffle: false, 
+      effect: "fade", 
+      duration: 1000, 
+      easing: "swing", 
+    });
+
+  // よくあるご質問スライドトグル
+  $('.q_box').click( function(){
+    $(this).next().slideToggle(300);
+  });
+
+  // メニュースムーズスクロール
+  $('a[href^="#"]').click(function(){
+    var speed = 500;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({scrollTop:position}, speed, "swing");
+    return false;
+    });
+});
